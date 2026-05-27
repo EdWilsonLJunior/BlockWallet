0a3e59abaf0bddc010eccee12755a54821adc109
ED WILSON LUCIANO JUNIOR <ed.luciano+bradesco@bradesco.com.br>
2026-05-27 07:59:33 -0300

    FirstCommit

---
+++ BlockWallet/BlockWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockWalletApp.swift
+//  BlockWallet
+//
+//  Created by Junior, Ed Wilson Luciano on 27/05/26.
+//
+
+import SwiftUI
+
+@main
+struct BlockWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
