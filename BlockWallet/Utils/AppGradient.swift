509429958e6e47feab43bb00aa2368c892dfddb4
ED WILSON LUCIANO JUNIOR <ed.luciano+bradesco@bradesco.com.br>
2026-06-16 13:16:20 -0300

    Altera cor

--- BlockWallet/Utils/AppGradient.swift
+++ BlockWallet/Utils/AppGradient.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct AppGradient {
 
     static let primary = LinearGradient(
-        colors: [Color.blue.mix(with: .black, by: 0.9), Color.blue.mix(with: .black, by: 0.7)],
+        colors: [Color.blue.mix(with: .black, by: 0.8), Color.blue.mix(with: .black, by: 0.7)],
         startPoint: .top,
         endPoint: .bottom
     )
